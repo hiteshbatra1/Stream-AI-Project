8ffdadb0da111d4398d1c238b30e2546aeb03783
hiteshbatra1 <hiteshbatra967@gmail.com>
2026-02-21 21:04:44 +0530

    feat: implement GitHub OAuth using Better Auth with sign-in page

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { currentUser } from "@/modules/authentication/actions";
+import UserButton from "@/modules/authentication/components/user-button";
+import React from "react";
+
+export default async function Home() {
+  const user = await currentUser();
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <UserButton user={user} />
+    </div>
+  );
+}
