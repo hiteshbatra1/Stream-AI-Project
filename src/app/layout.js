9f4507ba837478f6ae569b9cc59a42bba964f6dd
hiteshbatra1 <hiteshbatra967@gmail.com>
2026-02-24 17:29:40 +0530

    feat(chat): implement chat and message schema with initial chat creation flow

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { QueryProvider } from "@/components/providers/query-provider";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({ children }) {
             enableSystem
             disableTransitionOnChange
           >
+            <Toaster />
             {children}
           </ThemeProvider>
         </QueryProvider>
--- src/modules/chat/components/chat-message-form.jsx
+++ src/modules/chat/components/chat-message-form.jsx
@@ -6,9 +6,12 @@ import React, { useEffect, useState } from "react";
 import { ModelSelector } from "./model-selector";
 import { useAIModels } from "@/modules/ai-agent/hook/ai-agent";
 import { Spinner } from "@/components/ui/spinner";
+import { useCreateChat } from "../hooks/chat";
+import { toast } from "sonner";
 
 const ChatMessageForm = ({ initialMessage, onMessageChange }) => {
   const { data: models, isPending } = useAIModels();
+  const { mutateAsync, isPending: isChatPending } = useCreateChat();
   const [selectedModel, setSelectedModel] = useState(models?.models[0]?.id);
   const [message, setMessage] = useState("");
 
@@ -22,9 +25,13 @@ const ChatMessageForm = ({ initialMessage, onMessageChange }) => {
   const handleSubmit = async (e) => {
     try {
       e.preventDefault();
-      console.log("Message sent");
+      await mutateAsync({ content: message, model: selectedModel });
+      toast.success("Message sent successfully");
     } catch (error) {
-      console.log(error);
+      console.error("Error sending message", error);
+      toast.error("Failed to send message");
+    } finally {
+      setMessage("");
     }
   };
 
@@ -72,14 +79,22 @@ const ChatMessageForm = ({ initialMessage, onMessageChange }) => {
 
             <Button
               type="submit"
-              disabled={!message.trim()}
+              disabled={!message.trim() || isChatPending}
               size="sm"
               variant={message.trim() ? "default" : "ghost"}
               className="h-8 w-8 p-0 rounded-full"
             >
-              <Send className="h-4 w-4" />
+              {isChatPending ? (
+                <>
+                  <Spinner />
+                </>
+              ) : (
+                <>
+                  <Send className="h-4 w-4" />
 
-              <span className="sr-only">Send message</span>
+                  <span className="sr-only">Send message</span>
+                </>
+              )}
             </Button>
           </div>
         </div>
